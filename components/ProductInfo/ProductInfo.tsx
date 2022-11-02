a434fb9710a264dcfbfcfb305ee6ff71a1c23f81
Leonid <79872121399.yandex@gmail.com>
2022-11-02 07:48:26 +0300

    готовая вёрстка

---
+++ components/ProductInfo/ProductInfo.tsx
@@ -0,0 +1,24 @@
+import { useState } from 'react'
+import css from './../../styles/ProductInfo.module.sass'
+
+const ProductInfo = ({name, desc, id} : any) => {
+    const [isOpen, setIsOpen] = useState(false)
+
+    
+    const isOpenHandler = () => {
+        if(isOpen) setIsOpen(false)
+        else setIsOpen(true)
+    }
+
+    return <>
+        <div style={{borderTop: (id === 0)? 'solid 1px #dfdfdf' : '0'}} onClick={isOpenHandler} className={css.wrapper}>
+            <div className={css.nameWrapper}>
+                <h1>{name}</h1>
+                <div className={css.svgWrapper}><svg className={(isOpen)? css.open : css.close} width={'24'} focusable="false" viewBox="0 0 24 24" aria-hidden="true"><path fillRule="evenodd" clipRule="evenodd" d="m12.0003 15.5996-5.7857-5.785 1.414-1.4143 4.3717 4.3711 4.3717-4.3711 1.4141 1.4143-5.7858 5.785z"></path></svg></div>
+            </div>
+            {(isOpen)? <h2 onClick={isOpenHandler} className={css.desk}>{desc}</h2> : <></>}
+        </div>
+    </>
+}
+
+export default ProductInfo
--- pages/buy.tsx
+++ pages/buy.tsx
@@ -1,9 +1,59 @@
+import { useState } from "react"
+import ProductInfo from "../components/ProductInfo/ProductInfo"
+import CrossSvg from "../components/svg/CrossSvg"
+import css from './../styles/buy.module.sass'
+
+const productInfo = [{name: 'Размеры', desc: '100x100см'}, {name: 'Производитель', desc: 'ООО "Северный луч'}]
+
 const Buy = () => {
-    return (
-        <>
-            
+    const [isOpen, setIsOpen] = useState(false)
+
+    const modalHandler = () => {
+        if(!isOpen) setIsOpen(true)
+        else setIsOpen(false)
+    }
+
+    const Modal = () => {
+        if(isOpen) return <>
+            <div className={css.modalWrapper}>
+                <div className={css.modal}>
+                    <div>
+                        <div className={css.modalHeader}>
+                            <div className={css.logo}></div>
+                            <div onClick={modalHandler} className={'svgWrapper'}>
+                                <CrossSvg/>
+                            </div>
+                        </div>
+                        <h1>Форма заказа</h1>
+                        <input placeholder="Имя и фамилия"></input>
+                        <input placeholder="Электронная почта"></input>
+                        <input placeholder="Номер телефона"></input>
+                        <input placeholder="Почтовый индекс"></input>
+                    </div>
+                    <button onClick={modalHandler} className={css.button}>Отправить</button>
+                </div>
+            </div>
         </>
-    )
+    }
+
+
+    return <>
+        {Modal()}
+        <div className={css.slider}>
+            <div className={css.photo}></div>
+            <div className={css.buyWrapper}>
+                <div>
+                    <h1>Товар</h1>
+                    <h2>Краткое описание данного товара, например размеры или производитель</h2>
+                </div>
+                <div className={css.buy}>
+                    <h1>56250₽</h1>
+                    <button className={css.button} onClick={modalHandler}>Заказать</button>
+                </div>
+            </div>
+        </div>
+        {productInfo.map(({name, desc}, i) => <ProductInfo key={i} name={name} desc={desc} id={i}/>)}
+    </>
 }
 
 export default Buy
